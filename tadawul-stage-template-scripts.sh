383d6b962adb6ac4d09d5a8bff3ea591a918ccc9
destaitehB <destaiteh@kabi.ai>
2026-05-20 10:07:45 +0300

    Add tadawul stage template migration scripts

---
+++ tadawul-stage-template-scripts.sh
@@ -0,0 +1,13 @@
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/tadawul-Default-Template.json
+
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/tadawul-GDP-Template-Post.json
+
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/tadawul-Muqassa.json
+
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/tadawul-GDP-Template.json
+
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/tadawul-GDP-Program-2026.json
+
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/tadawul-Default-Template-same-document.json
+
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/tadawul-Default-Template-predefined-variables.json
