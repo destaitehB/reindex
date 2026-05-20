14e11cc1bbc2c9a92ce00a7fed07a26ad6765f74
destaitehB <destaiteh@kabi.ai>
2026-05-20 10:26:49 +0300

    Add MISK stage template JSON files and scripts

---
+++ misk-stage-template-scripts.sh
@@ -0,0 +1,3 @@
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/misk-2030-leaders-c8.json
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/misk-10x-leaders-c5.json
+npm run script file=1005_migrate_job_post_stages -- --url=https://raw.githubusercontent.com/destaitehB/reindex/refs/heads/main/misk-2030-leaders-c-8.json
